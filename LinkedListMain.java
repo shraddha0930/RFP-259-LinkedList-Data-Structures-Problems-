a04b1c75ecc1b861af34e4c28aaa802334728b82
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-09 22:40:36 +0530

    Passed:welcome message

---
+++ LinkedListMain.java
@@ -0,0 +1,7 @@
+package assignments.LinkedListProblems;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To LinkedList Data Structures Problem");
+    }
+}
