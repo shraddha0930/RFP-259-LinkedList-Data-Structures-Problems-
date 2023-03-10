a81f4154e221dba66ac38986c6ce7e0d48628389
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-10 17:23:56 +0530

    Added:code to create a simple linkedlist

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -1,7 +1,14 @@
 package assignments.LinkedListProblems;
 
+import java.util.LinkedList;
+
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome To LinkedList Data Structures Problem");
+        LinkedList<Integer> simpleLinkedList = new LinkedList<>();//create object
+        simpleLinkedList.add(56);
+        simpleLinkedList.add(30);
+        simpleLinkedList.add(70);
+        System.out.println("LinkedList:" +simpleLinkedList);
     }
 }
