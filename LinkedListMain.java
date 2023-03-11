da68fb5855158957dc0270bd07b0e7caa380f5a5
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-11 08:06:28 +0530

    Added:code to create a linkedlist by appending

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -1,15 +1,18 @@
 package assignments.LinkedListProblems;
 
-import java.util.LinkedList;
-
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome To LinkedList Data Structures Problem");
         SimpleLinkedList<Integer> simpleLinkedList = new SimpleLinkedList<>();//create object
+//        simpleLinkedList.add(70);
+//        simpleLinkedList.add(30);
+//        simpleLinkedList.add(56);
+//        simpleLinkedList.display();
+//        System.out.println();
 
-        simpleLinkedList.add(70);//call add method from simplelinkedlist and adding nodeOne to simplelinkedlist
-        simpleLinkedList.add(30);
         simpleLinkedList.add(56);
+        simpleLinkedList.append(30);
+        simpleLinkedList.append(70);
         simpleLinkedList.display();
 
     }
